e531f77c18c3b04f4cf0f9356b0cf09987806b73
Abhinav Tiwari <abhinavtiwari332@gmail.com>
2025-11-22 18:31:40 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,54 +1,41 @@
 variable "aws_region" {
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  type    = string
   default = "10.0.1.0/24"
 }
 
 variable "private_subnet_master_cidr" {
-  type    = string
   default = "10.0.2.0/24"
 }
 
 variable "private_subnet_replica_cidr" {
-  type    = string
   default = "10.0.3.0/24"
 }
 
-variable "key_name" {
-  type        = string
-  description = "EC2 key pair"
-}
+variable "key_name" {}
 
 variable "allowed_ssh_cidr" {
-  type    = string
-  default = "0.0.0.0/0" 
+  default = "0.0.0.0/0"
 }
 
 variable "redis_instance_type" {
-  type    = string
   default = "t3.micro"
 }
 
 variable "redis_port" {
-  type    = number
   default = 6379
 }
 
 variable "owner" {
-  type    = string
   default = "Abhinav"
 }
 
 variable "env" {
-  type    = string
   default = "dev"
 }
