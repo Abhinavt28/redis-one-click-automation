4839c4b9692d939a4d8e59fe4c06404df9671c2d
Abhinav Tiwari <abhinavtiwari332@gmail.com>
2025-11-22 17:50:42 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,64 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "vpc_id" {
-  description = "VPC ID where EC2 will be launched"
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "subnet_id" {
-  description = "Subnet ID for EC2"
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_master_cidr" {
+  description = "CIDR for Redis master subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_replica_cidr" {
+  description = "CIDR for Redis replica subnet"
+  type        = string
+  default     = "10.0.3.0/24"
 }
 
 variable "key_name" {
-  description = "AWS Key Pair name"
+  description = "Existing AWS key pair name"
+  type        = string
 }
 
-variable "instance_type" {
-  default = "t3.micro"
+variable "allowed_ssh_cidr" {
+  description = "CIDR from which SSH to bastion is allowed"
+  type        = string
+  default     = "0.0.0.0/0" # CHANGE THIS to your IP for security
 }
 
-variable "instance_name" {
-  default = "redis-server"
+variable "redis_instance_type" {
+  description = "Instance type for Redis EC2"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "redis_port" {
-  default = 6379
+  description = "Redis port"
+  type        = number
+  default     = 6379
 }
 
 variable "owner" {
-  default = "Abhinav"
+  description = "Owner tag"
+  type        = string
+  default     = "Abhinav"
 }
 
 variable "env" {
-  default = "dev"
+  description = "Environment tag"
+  type        = string
+  default     = "dev"
 }
-
