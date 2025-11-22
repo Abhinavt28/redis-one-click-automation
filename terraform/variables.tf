5a6a571de6c80278b79a417202be75ec899f55ea
Abhinav Tiwari <abhinavtiwari332@gmail.com>
2025-11-22 18:00:42 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,64 +1,54 @@
 variable "aws_region" {
-  description = "AWS region to deploy into"
   type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  description = "CIDR block for public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
+  type    = string
+  default = "10.0.1.0/24"
 }
 
 variable "private_subnet_master_cidr" {
-  description = "CIDR for Redis master subnet"
-  type        = string
-  default     = "10.0.2.0/24"
+  type    = string
+  default = "10.0.2.0/24"
 }
 
 variable "private_subnet_replica_cidr" {
-  description = "CIDR for Redis replica subnet"
-  type        = string
-  default     = "10.0.3.0/24"
+  type    = string
+  default = "10.0.3.0/24"
 }
 
 variable "key_name" {
-  description = "Existing AWS key pair name"
   type        = string
+  description = "EC2 key pair"
 }
 
 variable "allowed_ssh_cidr" {
-  description = "CIDR from which SSH to bastion is allowed"
-  type        = string
-  default     = "0.0.0.0/0" # CHANGE THIS to your IP for security
+  type    = string
+  default = "0.0.0.0/0" 
 }
 
 variable "redis_instance_type" {
-  description = "Instance type for Redis EC2"
-  type        = string
-  default     = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
 variable "redis_port" {
-  description = "Redis port"
-  type        = number
-  default     = 6379
+  type    = number
+  default = 6379
 }
 
 variable "owner" {
-  description = "Owner tag"
-  type        = string
-  default     = "Abhinav"
+  type    = string
+  default = "Abhinav"
 }
 
 variable "env" {
-  description = "Environment tag"
-  type        = string
-  default     = "dev"
+  type    = string
+  default = "dev"
 }
