238adb392f77a2a50bf4b755011b26eefda77273
Abhinav Tiwari <abhinavtiwari332@gmail.com>
2025-11-23 16:03:51 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,14 @@
 output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
+  description = "Public IP of Bastion Host"
+  value       = aws_instance.bastion.public_ip
 }
 
 output "redis_master_private_ip" {
-  value = aws_instance.redis_master.private_ip
+  description = "Private IP of Redis Master"
+  value       = aws_instance.redis_master.private_ip
 }
 
 output "redis_replica_private_ip" {
-  value = aws_instance.redis_replica.private_ip
+  description = "Private IP of Redis Replica"
+  value       = aws_instance.redis_replica.private_ip
 }
