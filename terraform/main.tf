7b1a4004e8c20772c0c9e735c9bac045a6cba9ad
Abhinav Tiwari <abhinavtiwari332@gmail.com>
2025-11-23 13:28:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -312,17 +312,4 @@ resource "aws_instance" "redis_replica" {
   EOF
 }
 
-# -----------------------------
-# OUTPUTS
-# -----------------------------
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
-}
 
-output "redis_master_private_ip" {
-  value = aws_instance.redis_master.private_ip
-}
-
-output "redis_replica_private_ip" {
-  value = aws_instance.redis_replica.private_ip
-}
